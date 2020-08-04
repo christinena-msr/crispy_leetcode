b73af243f99003fa36f9ec28d513c62f5934225f
crisypsodium <christinena.316@gmail.com>
2020-08-04 12:15:35 -0700

    updated notes

--- notes/keywords.js
+++ notes/keywords.js
@@ -3,7 +3,7 @@ break;
 break //label 
 
 switch(word) {
-    // each case can declare block-scoped variables (let & const)
+    // each case can declare block-scoped variables (let & const) as long as you add {} --> case: "string" {}
     case "category": // word must match category string case
         //do this
         break;
@@ -76,3 +76,29 @@ for (let i = 0; i < 10; i++) {
 // code
 debugger;
 // more code 
+
+// THIS IS VERY COOL
+// removes property from an object or array
+const Person = {
+    name: "Name"
+};
+delete Person.name;
+delete Person[name];
+// returns bool: successful -> true, else false
+// does not directly free memory
+// any var cannot be deleted from global scope or function's scope
+// any let or const cannot be deleted from defined scope
+// cannot remove properties of built-in objects like Math, Array, Object
+var trees = ["redwood", "bay", "cedar", "oak"];
+delete trees[2];
+// trees = ["redwood", "bay", "oak"]
+        // trees[0] trees[1] trees[3]
+if (2 in trees) {
+    //false and will not execute
+}
+trees[2] = undefined;
+if (2 in trees) {
+    //true and will execute
+}
+trees.splice(2, 1);
+//restructures array and removes trees[2] completely
