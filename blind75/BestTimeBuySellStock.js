49c4500a7aad64704d6f3d39167e4ad664237141
crispysodium <christinena.316@gmail.com>
2020-09-11 16:46:10 -0700

    added new solved problem

--- blind75/BestTimeBuySellStock.js
+++ blind75/BestTimeBuySellStock.js
@@ -2,32 +2,48 @@
  * @param {number[]} prices
  * @return {number}
  */
-// array of stock price on day i 
-// max profit for at most 1 transaction (buy or sell) per day
-// must buy before sell
-// example 1: [7, 1, 5, 3, 6, 4]
-// output: 5 
-// explanation: buy on day 2, sell on day 5 for max profit of 6 - 1 = 5
-var maxProfit = function(prices) {
-    // keep track of buy - sell = profit for each combination of days
-    // buy days must be chronological
-    // brute force would be O(n^2) with nested loops comparing each pair 
-    // if ( index of min < index of max) {
-    //     return max - min
-    // }
-    if ( prices.indexOf(Math.min(prices)) < prices.indexOf(Math.max(prices)) ){
-        return Math.max(prices) - Math.min(prices);
+var bruteForceMaxProfit = function(prices) {
+    let max = 0;
+    for (let i = 0; i < prices.length - 1; i++) {
+        for (let j = i + 1; j < prices.length; j++) {
+            let profit = prices[j] - prices[i];
+            if(profit > 0) {
+                max = Math.max(max, profit);
+            }
+        }
     }
-    // min buy is after max sell
-    const sellMax = prices.slice(0, prices.indexOf(Math.max(prices)) + 1);
-    const buyMin = prices.slice(prices.indexOf(Math.min(prices)), prices.length);
-    // find index of max
-    // loop from 0 to indexOf(max)
-    // capture max profit 
-    // find index of min
-    // loop from min to end 
-    // capture max profit 
+    return max;
 };
+
+// results:
+// runtime: 276 ms
+// memory: 37.6 MB
+// big O : O(n^2)
+
+const betterMaxProfit = function(prices) {
+    let minPrice = Number.MAX_SAFE_INTEGER;
+    let maxProfit = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if(prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+    return maxProfit;
+};
+
+// results: 98.49%
+// runtime: 68 ms
+// memory: 37.6 MB
+// big On: O(n)
 
 const example1 = [7, 1, 5, 3, 6, 4]
 const example2 = [7, 6, 4, 3, 1]
+const example3 = [1, 7, 5, 3, 6, 4]
+
+// console.log(betterMaxProfit(example1));
+// console.log(betterMaxProfit(example2));
+// console.log(betterMaxProfit(example3));
+
+console.log(Number.MAX_SAFE_INTEGER);
